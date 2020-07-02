70c72caf747e92eaebc26093e33bcadb2942e724
Dalen <dalen@m6.nz>
2020-07-01 19:31:40 -0600

    Initial Commit

---
+++ Simple Rsync/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Simple Rsync
+//
+//  Created by Dalen Ward on 7/1/20.
+//  Copyright © 2020 m6securities. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
